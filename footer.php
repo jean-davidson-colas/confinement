3faa3f4097f30b0b4290bd8be1355527842d24bb
colas <jean-davidson.colas@laplateforme.io>
2020-04-21 22:50:16 +0200

    debut de refonte css

--- footer.php
+++ footer.php
@@ -1,12 +1,4 @@
 <footer>
-<figure>
-    <figcaption>Naruto theme</figcaption>
-    <audio
-        controls autoplay
-        src="media/naruto-theme.mp3">
-            Your browser does not support the
-            <code>audio</code> element.
-    </audio>
-</figure>
+
 
 </footer>
