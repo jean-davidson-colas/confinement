2e5ce24b886257129005af9008580b1ef8a69e12
colas <jean-davidson.colas@laplateforme.io>
2020-04-24 10:27:23 +0200

    Projet Rush V 1.O

---
+++ creer-categories.php
@@ -0,0 +1,71 @@
+<?php
+	session_start();
+
+	$serverName = "localhost";
+    $userName = "root";
+    $passwordServer = "";
+    $nameTable = "confinement";
+
+    $connexion = mysqli_connect("$serverName", "$userName", "$passwordServer", "$nameTable") ;
+
+    
+
+    if (isset($_POST['addCat'])) 
+    {
+    	$newCat = $_POST['newcategorie'];
+    	$requeteNewCategorie = "INSERT INTO categories (nom) VALUES ('".$newCat."') ";
+    	$queryNewCategorie = mysqli_query($connexion, $requeteNewCategorie) ;
+    	header("location:index.php");
+
+    }
+
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Cree Categories</title>
+	<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+<div class="banniere">
+		<div class="logo">
+			<img src="img/logo.png">
+		</div>
+	</div>
+	<?php require 'header.php';?>
+
+	<main>
+		<?php
+			if ($_SESSION['id_droits'] == 1337 OR $_SESSION['id_droits'] == 42) 
+			{
+				
+			
+		
+		?>
+				
+				<section id="newCategorie">
+
+					<form action="creer-categories.php" method="post" id="formCategorie">
+						<label>Nom Categorie :</label>
+						<input type="text" name="newcategorie">
+						<br/>
+						<input type="submit" name="addCat" value="Ajouter">
+					</form>
+					
+				</section>
+		<?php
+			}
+			else
+			{
+				echo "Vous ne pouvez pas cree de categories";
+			}
+
+		?>
+	</main>
+
+	<?php require 'footer.php';?>
+
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,98 @@
+<header>
+    <?php
+date_default_timezone_set('UTC');
+
+
+if (isset($_SESSION['login']))
+ {
+    $login = $_SESSION['login'];
+    $today = date("d.m.y")
+?>
+<nav role="navigation">
+
+<div id="menu2">
+
+<input type="checkbox" />
+   
+   <span></span>
+   <span></span>
+   <span></span>
+    
+   <ul id="menu">
+
+
+        <?php 
+        if($_SESSION['id_droits'] == 1337)
+        {
+        ?>
+            <li><a href="index.php">Accueil</a></li>
+            <li><a href="News.php"> News</a></li>
+            <li><a href="admin.php">Admin</a>
+            <li><a href="profil.php">Profil</a>
+            <li><a href="allArticle.php">Article</a>
+            <li><a href="creer-article.php">Add Art.</a></li>
+            <li><a href="creer-categories.php">Add Cat.</a></li>
+            <li><a href="deconnexion.php">Déconnexion</a></li>
+        <?php
+        }
+        if ($_SESSION['id_droits'] == 42) 
+        {?>
+            <li><a href="index.php">Accueil</a></li>
+            <li><a href="News.php"> News</a></li>
+            <li><a href="modo.php">Modérateur</a>
+            <li><a href="profil.php">Profil</a>
+            <li><a href="allArticle.php">Article</a>
+            <li><a href="creer-article.php">Add Art.</a></li>
+            <li><a href="creer-categories.php">Add Cat.</a></li>
+            <li><a href="deconnexion.php">Déconnexion</a></li>
+        <?php
+        }
+        if ($_SESSION['id_droits'] == 1) 
+        {?>
+           <li><a href="index.php">Accueil</a></li>
+           <li><a href="News.php"> News</a></li>
+           <li><a href="allArticle.php">Article</a>
+            <li><a href="profil.php">Mon compte</a></li>
+            <li><a href="creer-article.php">Add Art.</a></li>
+            <li><a href="deconnexion.php">Déconnexion</a></li>
+        <?php
+        }
+        ?>
+        
+        
+
+    </ul>
+    </div>
+ </nav>
+<?php 
+}
+else
+ {
+?>
+
+<nav role="navigation">
+
+<div id="menu2">
+
+<input type="checkbox" />
+    <span></span>
+    <span></span>
+    <span></span>
+    
+    <ul id="menu">
+            <li><a href="index.php"> Accueil</a></li>
+            <li><a href="news.php"> News</a></li>
+            <li><a href="inscription.php"> Inscription</a></li>
+            <li><a href="connexion.php"> Connexion</a></li> 
+            
+            
+             
+     </ul>
+ </div>
+</nav>
+
+<?php
+ }
+?>
+
+</header>
